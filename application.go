tulir/mauview

commit 562ac8ffe3b3677467054eb971b2230bea2b687f

Add a custom mouse event wrapper around tcell's one
This wrapper implements the HasMotion() function which value is set by
the main application based on the previous mouse event.

application.go
@@ -38,6 +38,7 @@ type FocusableComponent interface {
 type Application struct {
 	screenLock   sync.RWMutex
 	screen       tcell.Screen
+	prevMouseEvt *tcell.EventMouse
 	root         Component
 	updates      chan interface{}
 	redrawTicker *time.Ticker
@@ -50,6 +51,7 @@ const queueSize = 255
 
 func NewApplication() *Application {
 	return &Application{
+		prevMouseEvt: &tcell.EventMouse{},
 		updates:      make(chan interface{}, queueSize),
 		redrawTicker: time.NewTicker(1 * time.Minute),
 		stop:         make(chan struct{}, 1),
@@ -132,7 +134,10 @@ func (app *Application) Start() error {
 					redraw = app.root.OnPasteEvent(customEvt)
 				}
 			case *tcell.EventMouse:
-				redraw = app.root.OnMouseEvent(event)
+				hasMotion := app.prevMouseEvt.Buttons() == event.Buttons()
+				customEvt := customMouseEvent{event, hasMotion}
+				app.prevMouseEvt = event
+				redraw = app.root.OnMouseEvent(customEvt)
 			case *tcell.EventResize:
 				clear = true
 				redraw = true

eventhandler.go
@@ -38,6 +38,15 @@ type PasteEvent interface {
 	Text() string
 }
 
+type customMouseEvent struct {
+	*tcell.EventMouse
+	motion bool
+}
+
+func (cme customMouseEvent) HasMotion() bool {
+	return cme.motion
+}
+
 // MouseEvent is an interface of the *tcell.EventMouse type.
 type MouseEvent interface {
 	tcell.Event

commit c8e4bd2b5851ebd0b31e3ddf99fd0a4a1f449c22

Fix scroll regression introduced in 562ac8ffe
Simply checking that the Buttons bitset of both events is the same is
not enough to set the motion to true. We have to make ture that the
event is only for buttons and not for the mouseweel.

application.go
@@ -134,7 +134,8 @@ func (app *Application) Start() error {
 					redraw = app.root.OnPasteEvent(customEvt)
 				}
 			case *tcell.EventMouse:
-				hasMotion := app.prevMouseEvt.Buttons() == event.Buttons()
+				onlyButtons := event.Buttons() < tcell.WheelUp
+				hasMotion := onlyButtons && app.prevMouseEvt.Buttons() == event.Buttons()
 				customEvt := customMouseEvent{event, hasMotion}
 				app.prevMouseEvt = event
 				redraw = app.root.OnMouseEvent(customEvt)